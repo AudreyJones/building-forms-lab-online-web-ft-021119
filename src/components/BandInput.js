19a88bf3739a66b0a41b68662a3ea1129b53d84a
AudreyJones <audrey.eliz.jones@gmail.com>
2019-07-17 09:55:47 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,40 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  state = {
+      name: ''
+  }
+
+  handleOnChange = event => {
+    // update this.state.name
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    // update Store's state! (calling dispatch)
+    this.props.addBand(this.state)
+    this.setState({
+      name: ''
+    })
+  }
+
+  
   render() {
     return(
       <div>
         Band Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text" 
+            value={this.state.name}
+            onChange={this.handleOnChange}/>
+          <input type="submit" />
+        </form>
+
       </div>
     )
   }
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
+import Bands from '../components/Bands'
+// Should be our Redux Store connection!!!!!
+
+
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <Bands bands={this.props.bands}/>
       </div>
     )
   }
